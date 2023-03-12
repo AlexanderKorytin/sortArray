7a40a79a7a5038427550c98a48689cb9b2c4614e
aleksandrkorytin <al.corytin@yandex.ru>
2023-03-12 23:12:56 +0300

    Ищем в введенном пользователем массиве два слагаемых введенного подльзователем числа/

--- src/FillArray.java
+++ src/FillArray.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
 public class FillArray implements FillArrayFromStr{
-    String[] strMass;
-    String str;
-    int request;
+
     Scanner scan = new Scanner(System.in);
-    public int requestСondition () {
+    public int request () {
+        int request;
         while (true) {
             System.out.println("Enter an integer, the terms of which will be searched in the array?");
             if (scan.hasNextInt()) {
@@ -21,7 +20,7 @@ public class FillArray implements FillArrayFromStr{
 
     @Override
     public Integer[] ArrayFromString(String str) {
-        strMass = str.split(" ");
+        String[] strMass = str.split(" ");
         Integer[] mass = new Integer[strMass.length];
         for (int i = 0; i < strMass.length; i++) {
             try {
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        FillArray fill = new FillArray();
-        String str;
+        FillArray fill;
+        fill = new FillArray();
         Scanner scanner = new Scanner(System.in);
         SortArrayInt sortArrays = new SortArrayInt();
         // читаем строку из консоли
         System.out.println("Enter an array in the form: x x x x");
-        str = scanner.nextLine();
+        String str = scanner.nextLine();
 
         Integer[] mass = fill.ArrayFromString(str); // преобразуем строку в массив чисел
-        int result = fill.requestСondition(); // получаем из консоли число два слагаемых которого буде искать в массиве
+        int result = fill.request(); // получаем из консоли число два слагаемых которого буде искать в массиве
 
         Integer[] sortMass = sortArrays.sort(mass);// сортируем массив по возрастанию, выводим его на экран
         Integer[] resultMass = sortArrays.searchResult(sortMass, result);// ищем слагаемые, введенного ранее сисла
